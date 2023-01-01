4ec640ca77f931e9bb64ef31f741ee7571cd0d30
Dumilde Matos <dmatos@cpiretail.pt>
2023-01-01 22:04:40 +0000

    first commit

---
+++ cedorator.ts
@@ -0,0 +1,17 @@
+// is usefull to allow objects to have different opctions 
+
+class ModelS {
+    public description = "Model S";
+
+    public cost() {
+        return 73000;
+    }
+}
+
+class ModelX {
+    public description = "Model X";
+
+    public cost() {
+        return 77000;
+    }
+}
